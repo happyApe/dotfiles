79c3131ff382e9ebfe76c7ad7659549a7dcae0ba
Anmol Jain <jain.anmol1119@gmail.com>
2021-08-09 19:34:53 +0530

    Another update

---
+++ templates/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+
+void solve() {
+ 
+ 
+}
+ 
+int32_t main() {
+ 
+    int T = 1;
+//    cin >> T;
+    while(T--) {
+        solve();
+    }
+ 
+	return 0;
+}
+ 
+ 
