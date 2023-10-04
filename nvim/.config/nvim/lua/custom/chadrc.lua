e160fd04b04ad60223a13203a84a1bdeddda7111
Anmol Jain <jain.anmol1119@gmail.com>
2023-10-04 10:51:56 -0400

    highlights and themes set

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,16 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'ayu_dark' }
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "decay",
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+  transparency = false,
+}
+
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
