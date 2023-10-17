9f1b20d733ce51634e0f4258d8780a6c386728db
Anmol Jain <jain.anmol1119@gmail.com>
2023-10-17 01:02:09 -0400

    better map and small change

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -48,7 +48,7 @@ M.general = {
     ["<leader>b"] = { "<cmd> enew <CR>", "New buffer" },
     ["<leader>ch"] = { "<cmd> NvCheatsheet <CR>", "Mapping cheatsheet" },
 
-    ["<leader>fm"] = {
+    ["<leader>fw"] = {
       function()
         vim.lsp.buf.format { async = true }
       end,
--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "chadracula",
+  theme = "dark_horizon",
   hl_override = highlights.override,
   hl_add = highlights.add,
   transparency = false,
